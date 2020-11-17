592974dcec14ca72a32acbca5900022233780b7a
Dialogika <dialogikadunia@gmail.com>
2020-11-17 19:18:21 +0700

    Bismillah

--- online/assets/vendor/wa/script.js
+++ online/assets/vendor/wa/script.js
@@ -1,5 +1,5 @@
 $(document).on('click','.send_form', function(){
-var input_blanter = document.getElementById('email');
+var input_blanter = document.getElementById('name');
 
 /* Whatsapp Settings */
 var walink = 'https://web.whatsapp.com/send',
@@ -16,21 +16,15 @@ var walink = 'whatsapp://send';
 if("" != input_blanter.value){
 
  /* Call Input Form */
-var input_select1 = $("#select :selected").text(), 
-    input_name1 = $("#name").val(),
-    input_email1 = $("#email").val(),
+var input_name1 = $("#name").val(),
     input_job1 = $("#job").val(),
     input_domisili1 = $("#domisili").val(),
-    input_umur1 = $("#umur").val(),
     input_message1 = $("#message").val();
 
 /* Final Whatsapp URL */
 var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
     '*Name* : ' + input_name1 + '%0A' +
-    '*Email Address* : ' + input_email1 + '%0A' +
-    '*Keperluan* : ' + input_select1 + '%0A' +
     '*Job* : ' + input_job1 + '%0A' +
-    '*Umur* : ' + input_umur1 + '%0A' +
     '*Domisili* : ' + input_domisili1 + '%0A' +
     '*Isi Message* : ' + input_message1 + '%0A';
 
