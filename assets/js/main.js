93088e82bba2f221f9489416e7a815260714a43e
Dialogika <dialogikadunia@gmail.com>
2022-03-04 20:47:32 +0700

    bismillah

--- assets/js/main.js
+++ assets/js/main.js
@@ -329,19 +329,23 @@
    */
 
   let countdown = select('.countdown');
-  const output = countdown.innerHTML;
 
   const countDownDate = function() {
     let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
 
-    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    let weeks = Math.floor(timeleft / (1000 * 60 * 60 * 24 * 7));
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24) % 7);
     let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+    let output = countdown.getAttribute('data-template');
+    output = output.replace('%w', weeks).replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+    countdown.innerHTML = output;
   }
   countDownDate();
   setInterval(countDownDate, 1000);
+
+  
 
 })()
