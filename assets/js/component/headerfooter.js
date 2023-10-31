a381b5bb1d19bad95d81982afca49808de124624
Rama Anindya <ramaanindyaa@gmail.com>
2023-10-31 12:40:39 +0700

    tambah kelas private

--- assets/js/component/headerfooter.js
+++ assets/js/component/headerfooter.js
@@ -38,8 +38,8 @@ class Header extends HTMLElement {
                   <li><a href="../program/basic-plus.html">Basic Plus</a></li>
                   <li><a href="../program/kids-plus.html">Kids Plus</a></li>
                   <li class="dropdown-divider"></li>
-                  <li><a href="../program/basic-plus.html">Basic Private</a></li>
-                  <li><a href="../program/kids-plus.html">Basic Private</a></li>
+                  <li><a href="../program/basic-private.html">Basic Private</a></li>
+                  <li><a href="../program/kids-private.html">Kids Private</a></li>
                   <li class="dropdown-divider"></li>
                   <li><a href="../program/action-play.html">Action Play</a></li>
                   <li><a href="../program/kids-play.html">Kids Play</a></li>
